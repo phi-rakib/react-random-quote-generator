77ffc4b0573eb1643aa82fa2c49bcdbd893645e4
rakib <phi.rakib@gmail.com>
2021-05-14 22:09:28 +0600

    added few more quotes

--- frontend/src/http/api/quotes.js
+++ frontend/src/http/api/quotes.js
@@ -27,4 +27,24 @@ export const quotes = [
     body: "Life is what happens when you're busy making other plans.",
     author: "John Lennon",
   },
+  {
+    body: "Live as if you were to die tomorrow. Learn as if you were to live forever",
+    author: "Mahatma Gandhi",
+  },
+  {
+    body: "Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.",
+    author: "Bernard M. Baruch",
+  },
+  {
+    body: "We must not allow other people’s limited perceptions to define us.",
+    author: "Virginia Satir",
+  },
+  {
+    body: "If you cannot do great things, do small things in a great way.",
+    author: "Napoleon Hill",
+  },
+  {
+    body: "Do not let what you cannot do interfere with what you can do.",
+    author: "ohn Wooden",
+  },
 ];
